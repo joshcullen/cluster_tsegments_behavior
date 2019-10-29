2f6d3da2ab9d9742aa716ed6b3b6d58a1420200b
Josh Cullen <joshcullen@Joshs-MacBook-Pro-3.local>
2019-10-29 12:00:41 -0400

    Update code to run real data

--- Step 5b_Cluster Behavior.R
+++ Step 5b_Cluster Behavior.R
@@ -11,15 +11,6 @@ source('gibbs sampler.R')
 #get data
 dat=read.csv('fake data.csv',as.is=T)
 
-#priors
-a.theta3=b.theta3=0.1
-psi=0.01
-gamma1=0.1
-
-#basic settings
-nclustmax=20
-ngibbs=1000
-nburn=ngibbs/2
 
 #run gibbs sampler
 res=cluster.tsegm.behavior(dat=dat,a.theta3=a.theta3,b.theta3=b.theta3,psi=psi,gamma1=gamma1,
